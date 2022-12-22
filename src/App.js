c5cd8f53fe668b7858b58e9b6021b98b23238199
ronentgreat <ronenh53@gmail.com>
2022-12-22 14:21:47 +0000

    completed container for large screens

--- src/App.js
+++ src/App.js
@@ -4,10 +4,13 @@ import Footer from './components/footer/footer.component';
 
 import Header from './components/header/header.component';
 
+import Container from './components/container/container.component';
+
 function App() {
   return (
     <div className="App">
       <Header />
+      <Container />
       <Footer />
     </div>
   );
---
+++ src/components/container/container.component.jsx
@@ -0,0 +1,50 @@
+import "./container.styles.css"
+
+import heroDesktop from "../../assets/images/image-hero-desktop.png"
+
+import heroMobile from "../../assets/images/image-hero-mobile.png"
+
+import databiz from "../../assets/images/client-databiz.svg";
+
+import audiophile from "../../assets/images/client-audiophile.svg";
+
+import meet from "../../assets/images/client-meet.svg";
+
+import maker from "../../assets/images/client-maker.svg";
+
+const Container  = () => {
+  return ( 
+    <main className="container">
+      <section className="main-section">
+        <picture>
+          <source media="(max-width:425px)" srcSet={heroMobile} />
+          <img style={{ width: "100%" }} src={heroDesktop} className="body-img" alt="web3 iamge" />
+        </picture>
+
+        <div className="content">
+          <h1 className="content__heading">Make remote work</h1>
+
+          <p className="content__paragraph">
+            Get your team in sync, no matter your location. Streamline processes,
+            create team rituals, and watch productivity soar.
+          </p>
+
+          <button className="content__btn">Learn more</button>
+
+
+          <div className="content__images">
+            <img src={databiz} alt="databiz" />
+            <img src={audiophile} alt="audiophile" />
+            <img src={meet} alt="meet" />
+            <img src={maker} alt="maker" />
+
+          </div>
+        </div>
+
+
+      </section>
+    </main>
+   );
+}
+ 
+export default Container;
