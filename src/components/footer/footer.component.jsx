5ac88d86605b9fa688c82fd2558f283b8b1549a0
ronentgreat <ronenh53@gmail.com>
2022-12-26 21:47:03 +0000

    fix warning of class name

--- src/components/footer/footer.component.jsx
+++ src/components/footer/footer.component.jsx
@@ -2,7 +2,7 @@ import "./footer.styles.css"
 
 const Footer = () => {
   return (
-    <footer class="attribution">
+    <footer className="attribution">
       Challenge by <a href="https://www.frontendmentor.io?ref=challenge" target="_blank" rel="noreferrer">Frontend Mentor</a>.
       Coded by <a href="https://twitter.com/Ronen_T_G">Ronen Hammond</a>.
     </footer>
